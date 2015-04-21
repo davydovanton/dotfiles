da7362bdd83a903c702be2442178e82d71c0af97
Anton Davydov <antondavydov.o@gmail.com>
2015-04-22 00:11:53 +0300

    Update post commit git hook

--- scripts/git_commit_file_analysis.sh
+++ scripts/git_commit_file_analysis.sh
@@ -11,28 +11,30 @@ call_reek() {
   reek $file
 }
 
-call_ruby_lint() {
-  echo "\n\nRuby Lint:"
-  ruby-lint $file
-}
-
 call_rubocop() {
     echo "\nRuboCop:"
     rubocop $file | tail -n +4 | sed '$d'
 }
 
+call_spellcheker() {
+    echo "\nCodespell:"
+    /Users/anton/bin/codespell/codespell.py $file
+}
+
 analyse_for_ruby_file() {
   ruby_file='^.+\.rb$'
   test_or_config_file='^(spec|test|config|db).+\.rb$'
+  schema_file='^.+schema.rb$'
   if [[ ${file} =~ $ruby_file ]]; then
     echo "\033[33;36mCHECKING: \033[33;12m" $file "\033[0m"
 
     if ! [[ ${file} =~ $test_file ]]; then
       call_flog $file
       call_reek $file
-      call_ruby_lint $file
     fi
-    call_rubocop $file
+    if ! [[ ${file} =~ $schema_file ]]; then
+      call_rubocop $file
+    fi
   fi
 }
 
@@ -40,4 +42,8 @@ files=`git diff-tree --no-commit-id --name-only -r HEAD`
 for file in $files
 do
   analyse_for_ruby_file $file
+  call_spellcheker $file
+  echo "\n"
 done
+
+/usr/local/bin/ctags -R *
